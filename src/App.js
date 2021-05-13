27dee7849c66cc80f544d48d3fac91a417757db6
Joseph-Reece <ndirashjose@gmail.com>
2021-05-13 08:49:59 +0300

    added UI feature

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   return (
     <div className="app">
       <div className="app__container">
+        <h2>Todo List</h2>
         <div className="app__todoContainer">
           {
             todoList.map((todo, idx) => (
